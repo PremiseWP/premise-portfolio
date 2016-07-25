0fe435c0aba10be8cd05ea1c46d4ba478a1fe8ff
mario vallejo <mario@vallgroup.com>
2016-07-25 16:53:48 -0500

    updated comments across the plugin. Releasable version ready

--- js/source/core.js
+++ js/source/core.js
@@ -3,25 +3,6 @@
  */
 ( function( $ ) {
 
-	// var ;
-
-	$( document ).ready( function() {
-
-		pwppBindEvents();
-
-	} );
-
-
-	function pwppBindEvents() {
-		( $( '#pwpp-portfolio-grid').length ) ? pwppsGridItemsSameHeight() : false;
-	}
-
-
-
-	function pwppsGridItemsSameHeight() {
-		$( window ).load( function() {
-			premiseSameHeight( '#pwpp-portfolio-grid .pwpp-item' );
-		} );
-	}
+	// JS will go here
 
 } ( jQuery ) );
