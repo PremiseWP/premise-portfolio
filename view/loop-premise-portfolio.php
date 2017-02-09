3e4d8bbbd7a3b95eac9e564d0cefa6a09cddc69d
Mario Vallejo <mario@vallgroup.com>
2017-02-09 17:07:19 -0600

    added new classes, organized spacing, and changed the thumbnail for the grid to call new image size.

--- view/loop-premise-portfolio.php
+++ view/loop-premise-portfolio.php
@@ -15,47 +15,56 @@
 
 	<div class="pwpp-container premise-clear-float">
 
-		<?php
-		if ( pwpp_have_posts() ) : ?>
+		<?php if ( pwpp_have_posts() ) : ?>
+
 			<div class="pwpp-the-loop">
-				<div <?php pwpp_loop_class(); ?>><?php
-					while ( pwpp_have_posts() ) { pwpp_the_post(); ?>
+				<div <?php pwpp_loop_class(); ?>>
 
-						<div <?php pwpp_loop_item_class(); ?>>
-								<div class="pwpp-item-inner">
-									<?php if ( '' !== get_the_title() && ! premise_get_value( 'pwpp_portfolio[loop][hide][title]' ) ) : ?>
-										<div class="pwpp-post-title">
-											<a href="<?php the_permalink(); ?>">
-												<h2><?php the_title(); ?></h2>
-											</a>
-										</div>
-									<?php endif; ?>
-									<a href="<?php the_permalink(); ?>">
-										<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][thumbnail]' ) ) pwpp_the_thumbnail( 'loop' ); ?>
-									</a>
-									<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][excerpt]' ) ) : ?>
-										<div class="pwpp-post-excerpt">
-											<?php
-											if ( premise_get_value( 'pwpp_portfolio[loop][excerpt]' ) ) {
-												the_excerpt();
-											}
-											else {
-												the_content();
-											} ?>
-										</div>
-									<?php endif; ?>
-									<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][meta]' ) ) : ?>
-										<div class="pwpp-post-meta">
-											By: <?php the_author(); ?>
-										</div>
-									<?php endif; ?>
-								</div>
-						</div>
+					<?php while ( pwpp_have_posts() ) : pwpp_the_post(); ?>
 
-					<?php
-					} ?>
+							<div <?php pwpp_loop_item_class( 'pwpp-item' ); ?>>
+									<div class="pwpp-item-inner">
+
+										<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][title]' ) ) : ?>
+											<div class="pwpp-post-title">
+												<a href="<?php the_permalink(); ?>">
+													<h3><?php the_title(); ?></h3>
+												</a>
+											</div>
+										<?php endif; ?>
+
+										<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][thumbnail]' ) ) : ?>
+											<div class="pwpp-post-thumbnail">
+												<a href="<?php the_permalink(); ?>">
+													<?php the_post_thumbnail( 'pwpp-loop-thumbnail', array( 'class' => 'pwp-responsive' ) ); ?>
+												</a>
+											</div>
+										<?php endif; ?>
+
+										<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][excerpt]' ) ) : ?>
+											<div class="pwpp-post-excerpt">
+												<?php ( premise_get_value( 'pwpp_portfolio[loop][excerpt]' ) )
+													? the_excerpt()
+													: the_content(); ?>
+											</div>
+										<?php endif; ?>
+
+										<?php if ( ! premise_get_value( 'pwpp_portfolio[loop][hide][meta]' ) ) : ?>
+											<div class="pwpp-post-meta">
+												<div class="pwpp-author">By: <?php the_author(); ?></div>
+												<div class="pwpp-cats">Categories: <?php the_terms( $post->ID, 'premise-portfolio-category', '', ', ' ); ?></div>
+												<div class="pwpp-tags">Tags: <?php the_terms( $post->ID, 'premise-portfolio-tag', '', ', ' ); ?></div>
+											</div>
+										<?php endif; ?>
+
+									</div>
+							</div>
+
+					<?php endwhile; ?>
+
 				</div>
 			</div>
+
 		<?php endif; ?>
 
 	</div>
